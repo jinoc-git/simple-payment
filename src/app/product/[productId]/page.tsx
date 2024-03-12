c3d81ea652263bb3f8fb53548c510aee3c8200bf
jinoc <choiae369@gmail.com>
2024-03-12 14:31:06 +0900

    Feat: product detail page 추가

---
+++ src/app/product/[productId]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Product from '@/components/product/Product';
+
+export default function ProductDetail() {
+  return (
+    <main className=" w-full min-h-[calc(100vh-48px)] bg-gray-100">
+      <div className="flex-box w-full h-[60px]">
+        <h2 className="text-2xl font-bold text-gray-700">상품 상세</h2>
+      </div>
+      <div className="flex justify-center gap-5 p-5">
+        <Product />
+      </div>
+    </main>
+  );
+}
