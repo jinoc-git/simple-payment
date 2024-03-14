e3a39e824078d8970f2abba7a75cb19120ced0f9
jinoc <choiae369@gmail.com>
2024-03-14 13:57:14 +0900

    Feat: deliveryAmount를 orderStore에 추가

--- src/store/orderStore.ts
+++ src/store/orderStore.ts
@@ -6,23 +6,30 @@ interface OrderStore {
   orderList: ProductType[];
   countList: string[];
   coupon: string | null;
+  deliveryAmount: number;
   orderPrice: number;
   afterCouponPrice: number;
   finalPrice: number;
+  addDeliveryPrice: (price: number) => void;
   setOrderPrice: (price: number) => void;
   setAfterCouponPrice: (price: number) => void;
   setFinalPrice: (price: number) => void;
   setCoupon: (coupon: string) => void;
 }
 
-export const orderStore = create<OrderStore>((set) => {
+export const orderStore = create<OrderStore>((set, get) => {
   return {
     orderList: [],
     countList: [],
     coupon: null,
+    deliveryAmount: 0,
     orderPrice: 0,
     afterCouponPrice: 0,
     finalPrice: 0,
+    addDeliveryPrice: (price: number) => {
+      const deliveryAmount = get().deliveryAmount + price;
+      set({ deliveryAmount });
+    },
     setOrderPrice: (price: number) => {
       set({ orderPrice: price });
     },
