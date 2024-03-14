05aaec53548dc1ccecc8d243b5014b4fca67d8fc
jinoc <choiae369@gmail.com>
2024-03-14 15:57:22 +0900

    Feat: 최종 결제 금액
    - 최종 결제 금액 콘텐츠 scrollY가 100 초과면 상단 고정

--- src/components/orderSheet-page/payments/paymentsInfo/PaymentsInfo.tsx
+++ src/components/orderSheet-page/payments/paymentsInfo/PaymentsInfo.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import OrdersheetContent from '@/components/common/layouts/ordersheetContent/OrdersheetContent';
 import { CardFooter } from '@/components/ui/card';
+import { addCommas } from '@/lib/changeNumberFormat';
 import { orderStore } from '@/store/orderStore';
 
 import Desc from './desc/Desc';
@@ -11,19 +12,41 @@ import Desc from './desc/Desc';
 const PaymentsInfo = () => {
   const { deliveryAmount, orderPrice, coupon, usingPoint, finalPrice } =
     orderStore();
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsFixed(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <OrdersheetContent
-      title="최종 결제 금액"
-      footer={<CardFooter></CardFooter>}
-    >
-      <Desc name="상품 가격" content={`${orderPrice}원`} />
-      <Desc name="쿠폰 할인" content={`-${coupon.discount}원`} />
-      <Desc name="포인트 사용" content={`-${usingPoint}원`} />
-      <Desc name="배송비" content={`${deliveryAmount}원`} />
-      <p>배송비 제외: {finalPrice}</p>
-      <p>최종: {deliveryAmount + finalPrice}</p>
-    </OrdersheetContent>
+    <div className={`${isFixed ? ' fixed top-5' : ' static'} w-[420px]`}>
+      <OrdersheetContent
+        title="최종 결제 금액"
+        footer={
+          <CardFooter>
+            <div className=" flex items-center justify-between w-full">
+              <p className=" font-semibold">최종 결제 금액: </p>
+              <p className=" text-xl font-semibold text-red-500">
+                {addCommas(deliveryAmount + finalPrice)}원
+              </p>
+            </div>
+          </CardFooter>
+        }
+      >
+        <Desc name="상품 가격" content={`${addCommas(orderPrice)}원`} />
+        <Desc name="쿠폰 할인" content={`-${addCommas(coupon.discount)}원`} />
+        <Desc name="포인트 사용" content={`-${addCommas(usingPoint)}원`} />
+        <Desc name="배송비" content={`${addCommas(deliveryAmount)}원`} />
+      </OrdersheetContent>
+    </div>
   );
 };
 
