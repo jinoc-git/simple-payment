cb82b9e1f382bc99d021a9474cf86cfa8cae5ea0
jinoc <choiae369@gmail.com>
2024-03-20 20:56:09 +0900

    Refactor: 폴더 구조 변경

--- src/app/productDetail/[productId]/page.tsx
+++ src/app/productDetail/[productId]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Product from '@/components/product/Product';
+import Product from '@/components/productDetail-page/Product';
 import { getProductById } from '@/lib/serverActions';
 
 interface ProductDetailProps {
